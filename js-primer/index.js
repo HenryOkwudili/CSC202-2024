98405641a434b641eca6387953ca7c59483c819b
HenryOkwudili <henryokwudili0@gmail.com>
2024-05-15 22:21:46 +0100

    first few weeks

---
+++ js-primer/index.js
@@ -0,0 +1,88 @@
+/*console.log('testing')
+
+ ASSIGNING AND PRINTING VARIABLES
+var a;
+a = 20;
+b = 11;
+var c = 12;
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+a = 13; //Reassign a new value of type number to a.
+console.log(a); //Prints out 13.
+a = "Another value which is a string"; //Reassign a new value of type string to a.
+console.log(a); //Prints out Another value which is a string.
+a = true; //Reassign a new value of type boolean - i.e. true or false.
+console.log(a); //Prints out true
+
+var d;
+console.log(d); //This will print out the value undefined.
+a = null;
+console.log(a); //This will print out the value null. 
+
+
+a = 10; //Assign variable a the value of 10
+b = 20; //Assign variable b the value of 20
+c = a + b; //Add value in a and b and put the result in variable c.
+console.log(c); //This should print out 30
+console.log(b - a); //Subtract a value from b value; displays 10 in console.
+console.log(b/a); //Divide b value by a value; displays 2 in console.
+console.log(a*b); //Multiply a value by b value; displays 200 in console.
+console.log(b%a); //Divide b value by a value and return remainder; displays 0 in console.
+console.log(a**2); //a value raised to power 2; displays 100 in console.
+
+  INCREMENTING AND DECREMENTING VARIABLES
+a++; //Increment a value.
+console.log(a); //Displays 11 in console.
+b--; //Decrement b value.
+console.log(b); //Displays 19 in console. 
+
+   STRING CONCATENATION
+firstName = "Henry";
+lastName = "Okwudili";
+fullName = firstName + " " + lastName; //Concatenate firstName and lastName
+console.log(fullName);//This should output the full name Henry Okwudili 
+
+a = 10;
+a < 11; //Returns true
+console.log(a < 11); //Displays true in console
+var test = a > 20; //Declares a variable named test and gives it the value of false.
+console.log(test); //Displays false in console
+a >= 10; //Returns true
+a <= 20; //Returns true
+a == 10; //Returns true
+a === "10"; //Returns false because a holds an integer value of 10 and not string value "10" 
+
+        //LOGICAL OPERATIONS
+a = 1;
+b = 2;
+console.log(a < 3 && b < 3); //Prints true as both a and b contain values that are less than 3.
+console.log(a > 3 && b < 3); //Prints false as the first condition (a > 3) is not true.
+console.log(a > 3 || b < 3); //Prints true as at least one condition (b < 3) is true.
+console.log(!(a < 3)); //Prints false as condition (NOT a < 3) is false.
+console.log(!(a < 3) || !(b > 3)); //Prints true as one of the conditions (NOT b > 3) is true. 
+
+// BITWISE OPERATIONS
+var a = 5;
+var b = 40;
+
+var c = a << 1; //Equivalent of 5 * 2
+console.log(c);
+var d = a <<2; //Equivalent of 5 * 4
+console.log(d);
+var e = a <<3; //Equivalent of 5 * 8
+console.log(e);
+var f = b >> 1; //Equivalent of 40 / 2
+console.log(f);
+var g = b >>2; //Equivalent of 40 / 4
+console.log(g);
+var h = b >>3; //Equivalent of 40 / 8
+console.log(h); */
+
+typeof "Henry Okwudili"; //Returns the value "string"
+typeof false; //Returns the value "boolean"
+typeof (10 + 10); //Returns the value "number"
+typeof 33; //Returns the value "number" 
+
---
+++ js-primer/loops.js
@@ -0,0 +1,13 @@
+for (var c = 1; c < 11; c++){
+    console.log(c);
+}
+
+for (var c = 1; c < 11; c++){
+    if(c == 7) break;
+    console.log(c);
+ } 
+
+ for (var c = 1; c < 11; c++){
+    if(c == 7) continue;
+    console.log(c);
+} 
